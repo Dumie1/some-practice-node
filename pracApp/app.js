db59c01f4c790960a8d21142d772188a8e28f0d1
Dumie1 <dumiedumara@gmail.com>
2018-10-04 10:56:48 +0200

    added new node folder

---
+++ pracApp/app.js
@@ -0,0 +1,13 @@
+// function sayHallo(name) {
+//     console.log('hey ' + name);
+// }
+// sayHallo('Dee');
+
+/*
+Modules build for Node
+- Operating system.
+- file system (fs)
+- events
+- http
+*/
+console.log(module);
