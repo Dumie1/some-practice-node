a3a9232a405e11aa982bfc0f5f7741d9cf2b8a49
Dumie1 <dumiedumara@gmail.com>
2018-10-05 17:37:57 +0200

    added SOME CODE

--- pracApp/app.js
+++ pracApp/app.js
@@ -5,8 +5,23 @@ Modules build for Node
 - events
 - http
 */
+/*steps in creating this project
 
+*/
 const express = require('express');
 const app = express();
 
- 
+app.get('/', (req, res) => { 
+    res.send('My new App~~!!')
+}); 
+
+app.get('/api/courses', (req, res) => {
+    res.send([1, 2, 3]);
+});
+
+//Environmental variable
+
+const port = process.env.PORT || 3000;
+
+
+app.listen(port, () => console.log(`Listening on port ${port}...`));
