0c031ccdd2c312b608117dbae9ecfcb40af5b065
Dumie1 <dumiedumara@gmail.com>
2018-10-06 15:49:03 +0200

    added new argv file

---
+++ processArgv/index.js
@@ -0,0 +1,20 @@
+const arg1 = parseInt( process.argv[2]);
+const arg2 = process.argv[3];
+const arg3 = parseInt(process.argv[4]);
+
+if (arg2 == '+') {
+    result = arg1 + arg3;
+} else if (arg2 == '-') {
+    result = arg1 - arg3;
+} else if (arg2 == '/') {
+    result = arg1 / arg3;
+} else if (arg2 == '*' ) {
+    result = arg1 * arg3;
+}
+
+console.log('The answer is ' + result);
+
+/*
+to run the program - type node index.js 2 + 3 the press enter
+use parse int to change string to a number
+*/
