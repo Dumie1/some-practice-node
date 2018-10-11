f364df2afd6571530d4baf79718226f518926526
Dumie1 <dumiedumara@gmail.com>
2018-10-11 12:20:17 +0200

    updated description.js

--- week1/h-week1/src/App.js
+++ week1/h-week1/src/App.js
@@ -25,6 +25,8 @@ class App extends Component {
       
         
         <Description />
+      
+
         <hr/>
     
       </div>
--- week1/h-week1/src/Description.js
+++ week1/h-week1/src/Description.js
@@ -18,21 +18,6 @@ class Description extends Component {
         });
     }
 
-    // renderTodos() {
-    //     if (this.state.todos.length === 0) return <p>No items...</p>
-        
-    // }
-    //     todo = todos.map((todos, id) => (
-
-    //         <li key={id}>
-    //             <input checked={todos.done} type='checkbox' onChange={this.isTaskDone.bind(this, todos)}/>
-    //             {`${todos.description} , ${todos.deadline}`}
-    //         </li>
-    //     ));
-
-
-
-
     renderTodos() {
         if (this.state.todos.length === 0) return <p>No items...</p>
 
@@ -64,62 +49,3 @@ class Description extends Component {
 }
 
 export default Description;
-
-
-
-
-
-
-
-
-//---------------------------------------------
-  
-// class Description extends Component {
-//     state = {
-//         Todos
-//     }
-
-//     isTaskDone = (todo) => {
-//         console.log(todo);
-
-//         todo.done = !todo.done;
-//         this.setState({
-//             todos: this.state.todos
-//         });
-//     }
-
-//     renderTodos() {
-//         if (this.state.Todos.length === 0) return <p>No items...</p>
-
-//         const textStyle = { textDecoration: 'line-through' }
-
-
-//         return <ul>
-//             {
-//                 Todos.map((todo) => {
-
-//                     return (
-//                         <li key={todo.id}>
-//                             <input checked={todo.done} type='checkbox' onChange={this.isTaskDone.bind(this, todo)} />
-//                             <span style={todo.done ? textStyle : null}>
-//                                 {todo.description} , {todo.deadline}
-//                             </span>
-//                         </li>)
-//                 })
-//             }
-//         </ul>
-//     }
-
-//     render() {
-//         console.log(this.state.todos);
-//         return (
-//             <div className='items'>
-
-                
-//                 {this.renderTodos()}
-
-//             </div>
-//         );
-//     }
-// }
-// export default Description;
