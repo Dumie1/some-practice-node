2c7a29ab89b318facc46353c4bef3ddf60ba4806
Dumie1 <dumiedumara@gmail.com>
2018-10-25 12:22:54 +0200

    calling for help

--- reactPract/homework-test/src/Category.js
+++ reactPract/homework-test/src/Category.js
@@ -10,13 +10,14 @@ class Category extends React.Component {
     }
 
 
-    handleChange = (e) => {
-        this.setState({ description: e.target.value });
+    newTodoChanged =(e)=>{
+        console.log(e.target.value);
+        
     }
 
-    handleSubmit = (e) => {
+    formSubmitted = (e) => {
         e.preventDefault();
-        const { todos } = this.state;
+         const { todos } = this.state;
         //assemble data
         const newTodo = {
             id: todos.length + 1,
@@ -24,15 +25,17 @@ class Category extends React.Component {
             deadline: e.target.value,
             done: false
         }
-        //update state
+        //add new todo to list
         this.state.todos.push(newTodo);
         //update state
         this.setState({ todos });
     }
 
-    deleteTodo = (e, i) => {
+
+
+    deleteTodo = ( index) => {
         const todos = this.state.todos;
-        todos.splice(i, 1);
+        todos.splice(index, 1);
         this.setState({ todos });
     }
     
@@ -73,14 +76,14 @@ class Category extends React.Component {
             <div>
                 {/* <Form /> */}
 
-                <form onSubmit={this.handleSubmit}>
+                <form onSubmit={this.formSubmitted}>
                     <label>
                         Description :
-                    <input type="text" value={this.state.value} onChange={this.handleChange.bind(this)}/>
+                    <input onChange={this.newTodoChanged}type="text" name='description'/>
                     </label>
                     <label>
                         Deadline :
-                    <input type="date" value={this.state.deadline}/>
+                    <input type="date" name='deadline'/>
                     </label>
                     <input type="submit" value="Submit" />
                 </form>
@@ -96,104 +99,3 @@ class Category extends React.Component {
 }
 
 export default Category;
-
-
-// -----------reall code below
-
-// import React from 'react';
-// import todos from './todos.json';
-// import Form from './Form';
-
-// class Category extends React.Component {
-
-
-//     state = {
-//         todos
-//     }
-
-
-
-
-
-
-
-
-
-
-//     addTodo = e => {
-//         e.preventDefault();
-//         const { todos } = this.state;
-//         //assemble data
-//         const newTodo = {
-//             id: todos.length + 1,
-//             description: e.target.description.value,
-//             deadline: e.target.deadline.value,
-//             done: false
-//         }
-//         //update state
-//         this.state.todos.push(newTodo);
-//         //update state
-//         this.setState({ todos });
-//     }
-
-
-
-//     newTodoChanged = (e) => {
-//         this.setState({
-//             newTodo: e.target.value
-//         });
-
-//     }
-
-
-
-
-
-
-//     isTaskDone(todo) {
-//         todo.done = !todo.done;
-//         this.setState({
-//             todos: this.state.todos
-//         });
-//     }
-
-//     renderTodos() {
-//         if (this.state.todos.length === 0) return <p className='noItems'>No items...</p>
-
-//         const textStyle = { textDecoration: 'line-through', backgroundColor: '#0455d8' }
-
-//         return <ul>
-//             {
-//                 todos.map((todo, id) => {
-//                     return (
-//                         <li key={id}>
-//                             <input checked={todo.done} type='checkbox' onChange={this.isTaskDone.bind(this, todo)} />
-//                             <span style={todo.done ? textStyle : null}>
-//                                 {todo.description} , {todo.deadline}
-//                             </span>
-//                             <button onClick={this.editTodo} className='edit'>Edit</button>
-//                             <button onClick={this.removeTodo} className='remove'>Remove</button>
-
-//                         </li>
-
-//                     )
-//                 })
-//             }
-//         </ul>
-//     }
-
-//     render() {
-//         return (
-//             <div>
-//                 <Form />
-//                 <div className='items'>
-//                     {this.renderTodos()}
-
-//                 </div>
-
-//             </div>
-//         )
-//     }
-// }
-
-// export default Category;
--- reactPract/homework-test/src/Form.js
+++ reactPract/homework-test/src/Form.js
@@ -1,122 +1,23 @@
-// import React from 'react';
+import React from 'react';
 
-// class Form extends React.Component{
-
-    
-
-//     addItem = (e) => {
-//         e.preventDefault();
-        
-//     }
-
-//     render() {
-//         return (
-//             <form onSubmit={this.addItem}>
-//                 <input ref={(a) => this._inputElement = a}
-//                     placeholder="enter task">
-//                 </input>
-//                 <button type="submit">add</button>
-//             </form>
-//         )
-//     }
-// }
-
-// export default Form;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// ----------------second below
-
-// class Form extends React.Component {
-   
-//     justSubmitted(event) {
-//         event.preventDefault();
-//         let input = event.target.querySelector('input');
-//         let value = input.value;
-//         input.value = '';
-//         this.addTodo(value);
-//     }
-
-//     render() {
-//         return (   
-//             <form onSubmit={this.justSubmitted}>
-//                 <input type='text'></input>
-//                 <input type='date'></input>
-//             </form>
-//         );
-//     }
-// }
-
-// export default Form;
-
-
-
-//-------------------first below
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React from 'react';
-
-// class Form extends React.Component{
+class Form extends React.Component{
             
-//     render() {
-//         return (
-//             <form onSubmit={(e) => this.props.addTodo(e)}>
-//                 <label>
-//                         Enter description :
-//                      <input onChange={(e) => this.props.newTodoChanged(e)} type='text' name='description' placeholder='Enter a description...' />
-//                 </label>
-//                 <label>
-//                         Deadline :
-//                      <input type='date' name='name' />
-//                 </label>
-//                 <button type='submit' className='add'>Add</button>
-//             </form> 
+    render() {
+        return (
+            <form onSubmit={(e) => this.props.addTodo(e)}>
+                <label>
+                        Enter description :
+                     <input type='text' name='description' placeholder='Enter a description...' />
+                </label>
+                <label>
+                        Deadline :
+                     <input type='date' name='name' />
+                </label>
+                <button type='submit' className='add'>Add</button>
+            </form> 
 
-//                 )
-//             }
-// }
+                )
+            }
+}
 
-// export default Form;
+export default Form;
