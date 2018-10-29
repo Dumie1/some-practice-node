a1c910cd9eae01d268124f8d36de4180a574a132
Dumie1 <dumiedumara@gmail.com>
2018-10-30 00:14:23 +0100

    fixed delete function

--- reactPract/homework-test/src/Data.js
+++ reactPract/homework-test/src/Data.js
@@ -1,99 +1,104 @@
-import React from 'react';
-import Todos from './Todos.json';
-import Form from './Form';
+import React from "react";
+import Todos from "./Todos.json";
+import Form from "./Form";
 
 class Data extends React.Component {
+  state = {
+    Todos
+  };
 
-    state = {
-        Todos
-    }
+  descriptionInput = React.createRef();
+  deadlineInput = React.createRef();
 
+  addTodo = event => {
+    event.preventDefault();
 
-    descriptionInput = React.createRef();
-    deadlineInput = React.createRef();
+    const descriptionInput = this.descriptionInput.current.value;
+    const deadlineInput = this.deadlineInput.current.value;
+    const newState = this.state;
 
+    const Todos = newState.Todos;
+    Todos.push({
+      id: Todos.length + 1,
+      description: descriptionInput,
+      deadline: deadlineInput,
+      done: false
+    });
 
-    addTodo = (event) => {
-        event.preventDefault();
-        
-        const descriptionInput = this.descriptionInput.current.value;
-        const deadlineInput = this.deadlineInput.current.value;
+    this.setState(newState);
 
-        this.setState((prevState) => {
-            let todos = prevState.todos;
+    this.descriptionInput.current.value = "";
+    this.deadlineInput.current.value = "";
+  };
 
-            Todos.push({
-                id: Todos.length + 1,
-                description: descriptionInput,
-                deadline: deadlineInput,
-                done: false,
-            });
-            return { Todos: todos };
-        });
+  deleteTodo = id => {
+    const arr = this.state.Todos;
+    arr.splice(id, 1);
+    this.setState({ Todos: arr });
+  };
 
-        this.descriptionInput.current.value = '';
-        this.deadlineInput.current.value = '';
-    }
+  // deleteTodo = id => {
+  //   this.setState(prevState => {
+  //     let todos = prevState.todos;
+  //     Todos.splice(id, 1);
+  //     return { Todos: todos };
+  //   });
+  // };
 
+  isTaskDone = todo => {
+    todo.done = !todo.done;
+    this.setState({
+      todos: this.state.todos
+    });
+  };
 
-    deleteTodo = (id) => {
-        this.setState((prevState) => {
-            let todos = prevState.todos;
-            Todos.splice(id, 1);
-            return { Todos: todos };
-        });
-    }
+  renderTodos = () => {
+    if (this.state.Todos.length === 0)
+      return <p className="noItems">No items...</p>;
+    console.log(this.state);
+    const textStyle = {
+      textDecoration: "line-through",
+      backgroundColor: "#0455d8"
+    };
 
+    return (
+      <ul>
+        {Todos.map((todo, id) => {
+          return (
+            <li key={id}>
+              <input
+                checked={todo.done}
+                type="checkbox"
+                onChange={this.isTaskDone.bind(this, todo)}
+              />
+              <span style={todo.done ? textStyle : null}>
+                {todo.description} , {todo.deadline}
+              </span>
+              <button onClick={this.editTodo} className="edit">
+                Edit
+              </button>
+              <button onClick={event => this.deleteTodo(id)} className="remove">
+                Remove
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    );
+  };
 
-    isTaskDone = (todo) =>{
-        todo.done = !todo.done;
-        this.setState({
-            todos: this.state.todos 
-        });
-    }
-
-
-    renderTodos = () => {
-        // if (this.state.Todos.length === 0) return <p className='noItems'>No items...</p>
-
-        const textStyle = { textDecoration: 'line-through', backgroundColor: '#0455d8'}
-
-        return <ul>
-            {
-                Todos.map((todo, id) => {
-                    return (
-                        <li key={id}>
-                            <input checked={todo.done} type='checkbox' onChange={this.isTaskDone.bind(this, todo)} />
-                            <span style={todo.done ? textStyle : null}>
-                                {todo.description} , {todo.deadline}
-                            </span>
-                            <button onClick={this.editTodo} className='edit'>Edit</button>
-                            <button onClick={(event)=>this.deleteTodo(id)} className='remove'>Remove</button>
-                        </li>
-                           
-                    )
-                })
-            }
-        </ul>
-    }
-
-    render() {
-        
-        return (
-            
-             <div>
-                <Form
-                    addTodo={this.addTodo}
-                    descriptionInput={this.descriptionInput}
-                    deadlineInput={this.deadlineInput}
-                />
-                <div className='items'>
-                    {this.renderTodos()}
-                    
-                </div>
-             </div> 
-        )
-    }
+  render() {
+    return (
+      <div>
+        <Form
+          addTodo={this.addTodo}
+          descriptionInput={this.descriptionInput}
+          deadlineInput={this.deadlineInput}
+        />
+        <div className="items">{this.renderTodos()}</div>
+      </div>
+    );
+  }
 }
 
 export default Data;
@@ -103,31 +108,11 @@ export default Data;
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // reall code below--
-
 
 // import React from 'react';
 // import Todos from './Todos.json';
 // // import Form from './Form';
-
 
 // class Data extends React.Component {
 
@@ -161,16 +146,12 @@ export default Data;
 
 //     // --------refs end--
 
-
-
 //     state = {
 //         Todos
 //     }
 
-
 //     // newTodoChanged =(e)=>{
 //     //     console.log(e.target.value);
-
 
 //     // }
 
@@ -190,7 +171,6 @@ export default Data;
 //     //     this.setState({ todos });
 
 //     // }
-
 
 //     deleteTodo = (index) => {
 //         const todos = this.state.todos;
@@ -246,11 +226,8 @@ export default Data;
 //                 <input ref={this.todoInputDate} type="date" name='description' />
 //                     </label>
 
-
 //                     <input type="submit" value="Submit" />
 //                 </form>
-
-
 
 //                 {/* <form >
 //                     <label>
@@ -263,19 +240,15 @@ export default Data;
 //                 <input type="date" name='deadline'
 //                         ref={this.todoInputDate} onKeyUp={this.addTodo} />
 //                 </label>
-                    
+
 //                     <input type="submit" value="Submit" />
-            
+
 //                  <form/> */}
-
-
 
 //                 {/* <Form
 //                     submitForm={this.submitForm}
 //                     newTodoChanged={this.newTodoChanged}
 //                 /> */}
-
-
 
 //                 <div className='items'>
 //                     {this.renderTodos()}
